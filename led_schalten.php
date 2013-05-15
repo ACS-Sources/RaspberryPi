345ba468fda50e023b4b075f298e0e904507d586
ACS <git@acs-it-worx.de>
2013-05-15 12:51:57 +0200

    Pfadanpassung

--- led_schalten.php
+++ led_schalten.php
@@ -2,9 +2,9 @@
 	echo "Dies ist ein Test\n";
 	echo "In diesem Beispiel wird ein Python Skript ausgefuehrt.\n";
 	
-	$res = shell_exec("sudo /usr/bin/python /var/www/led_on.py");
+	$res = shell_exec("sudo /usr/bin/python /home/pi/PythonCode_Samples/led_schalten.py");
 	var_dump($res)."<p>";
 
-	$res = shell_exec("ls -la /var/www/");
+	$res = shell_exec("ls -la /home/pi/PythonCode_Samples/");
 	var_dump($res)."<p>";
 ?>
